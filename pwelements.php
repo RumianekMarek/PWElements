69febd332dac7c0268271c4cd035a705f3d65b97
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2025-01-31 08:52:55 +0100

    Add files via upload

--- pwelements.php
+++ pwelements.php
@@ -4,7 +4,7 @@
  * Plugin Name: PWE Elements
  * Plugin URI: https://github.com/RumianekMarek/PWElements
  * Description: Adding a PWE elements to the website.
- * Version: 2.4.8
+ * Version: 2.4.9
  * Author: Marek Rumianek
  * Author URI: github.com/RumianekMarek
  * Update URI: https://api.github.com/repos/RumianekMarek/PWElements/releases/latest
