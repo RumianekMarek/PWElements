2ea924225c1d555ea0918a3c1d8af23ef264473c
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2025-07-23 12:58:18 +0200

    Add files via upload

--- pwelements.php
+++ pwelements.php
@@ -27,7 +27,7 @@ class PWElementsPlugin {
     public $PWEProfile;
     public $PWEStore;
     public $PWEConferenceCap;
-    public $PWEIndustryEvening;
+    // public $PWEIndustryEvening;
     // public $PWELogoFetcher;
 
     public function __construct() {
@@ -166,8 +166,8 @@ class PWElementsPlugin {
             require_once plugin_dir_path(__FILE__) . 'includes/calendar/calendar.php';
             $this->PWECalendar = new PWECalendar();
 
-            require_once plugin_dir_path(__FILE__) . 'includes/conference-calendar/conference-calendar.php';
-            $this->PWEConferenceCalendar = new PWEConferenceCalendar();
+            // require_once plugin_dir_path(__FILE__) . 'includes/conference-calendar/conference-calendar.php';
+            // $this->PWEConferenceCalendar = new PWEConferenceCalendar();
         }
 
         // require_once plugin_dir_path(__FILE__) . 'includes/top10/pwelogofetcher.php';
@@ -177,8 +177,8 @@ class PWElementsPlugin {
         // require_once plugin_dir_path(__FILE__) . 'qr-active/main-qr-active.php';
         // $this->PWEQRActive = new PWEQRActive();
 
-        require_once plugin_dir_path(__FILE__) . 'includes/industry-evening/industry-evening.php';
-        $this->PWEIndustryEvening = new PWEIndustryEvening();
+        // require_once plugin_dir_path(__FILE__) . 'includes/industry-evening/industry-evening.php';
+        // $this->PWEIndustryEvening = new PWEIndustryEvening();
 
         require_once plugin_dir_path(__FILE__) . 'includes/reviews/reviews.php';
         $this->PWEReviews = new PWEReviews();
