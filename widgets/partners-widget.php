4d4de7d9375a97c64f280a77171bd66236b85e73
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2025-07-25 11:49:14 +0200

    Add files via upload

--- widgets/partners-widget.php
+++ widgets/partners-widget.php
@@ -1,5 +1,7 @@
 <?php
 
+$unique_id = PWECommonFunctions::id_rnd();
+
 $pwe_header_partners_title_color = !empty($pwe_header_partners_title_color) ? $pwe_header_partners_title_color : 'black';
 $pwe_header_partners_background_color = !empty($pwe_header_partners_background_color) ? $pwe_header_partners_background_color : 'rgb(190 190 190 / 80%)';
 
@@ -42,7 +44,7 @@ $output .= '
             max-width: 250px;
             text-align: center;
             margin: 16px auto 0;
-            font-size: 20px;
+            font-size: 18px;
         }
         .pwe-header-partners__items {
             display: flex;
@@ -76,38 +78,89 @@ $output .= '
         }
     </style>';
 
-    if (count($pwe_header_partners_items) > 2) {
+    $files = [];
+
+    if (!empty($pwe_header_partners_items)) {
+        foreach ($pwe_header_partners_items as $item) {
+            $partner_item_logo_url = wp_get_attachment_url($item);
+            $files[] = $partner_item_logo_url;
+        }
+    }
+
+    if (count($files) > 0) {
+        foreach ($files as &$file) {
+            $file = [
+                'url' => $file,
+                'desc_pl' => basename(dirname($file)),
+                'desc_en' => '',
+                'link' => ''
+            ];
+        }
+        unset($file);
+    }
+
+    $saving_paths = function (&$files, $logo_data) {
+        // Get desc_pl & desc_en from meta_data
+        $meta = json_decode($logo_data->meta_data, true);
+        $desc_pl = $meta["desc_pl"] ?? '';
+        $desc_en = $meta["desc_en"] ?? '';
+        $link = $logo_data->logos_link;
+
+        $element = [
+            'url' => 'https://cap.warsawexpo.eu/public' . $logo_data->logos_url,
+            'desc_pl' => $desc_pl,
+            'desc_en' => $desc_en,
+            'link' => $link
+        ];
+
+        // Adding logos_url to $files only if it is not already there
+        if (!in_array($element, $files)) {
+            $files[] = $element;
+        }
+    };
+
+    $cap_logotypes_data = PWECommonFunctions::get_database_logotypes_data();
+    if (!empty($cap_logotypes_data) && !empty($pwe_header_partners_catalog)) {
+        $logotypes_catalogs_array = explode(',', $pwe_header_partners_catalog);
+        foreach ($cap_logotypes_data as $logo_data) {
+            if (in_array($logo_data->logos_type, $logotypes_catalogs_array)) {
+                $saving_paths($files, $logo_data);
+            }
+        }
+    }
+
+    if (count($files) > 1) {
         $output .= '
         <style>
-            .pwe-header-partners-container {
+            .pwe-header-partners-container.partners-'. $unique_id .' {
                 max-width: 280px;
             }
-            .pwe-header-partners__items {
+            .partners-'. $unique_id .' .pwe-header-partners__items {
                 flex-wrap: wrap;
                 flex-direction: row;
                 gap: 8px;
             }
-            .pwe-header-partners__item {
+            .partners-'. $unique_id .' .pwe-header-partners__item {
                 max-width: 120px;
             }
             @media(max-width: 1200px) {
-                .pwe-header-partners-container {
+                .pwe-header-partners-container.partners-'. $unique_id .' {
                     max-width: 240px;
                 }
-                .pwe-header-partners__items {
+                .partners-'. $unique_id .' .pwe-header-partners__items {
                     flex-wrap: wrap;
                     flex-direction: row;
                     gap: 8px;
                 }
-                .pwe-header-partners__item {
+                .partners-'. $unique_id .' .pwe-header-partners__item {
                     max-width: 100px;
                 }
             }
             @media(max-width: 1100px) {
-                .pwe-header-partners-container {
+                .pwe-header-partners-container.partners-'. $unique_id .' {
                     max-width: 280px;
                 }
-                .pwe-header-partners__item {
+                .partners-'. $unique_id .' .pwe-header-partners__item {
                     max-width: 120px;
                 }
             }
@@ -115,62 +168,107 @@ $output .= '
     }
 
     $output .= ' 
-    <div class="pwe-header-partners">
+    <div class="pwe-header-partners partners-'. $unique_id .'">
         <div class="pwe-header-partners-container">
             <div class="pwe-header-partners__title">
                 <h3>'. $pwe_header_partners_title .'</h3>
             </div>
             <div class="pwe-header-partners__items">';
-                foreach ($pwe_header_partners_items as $item) {
-                    $partner_item_logo_url = wp_get_attachment_url($item);
-                    $output .= '
-                    <div class="pwe-header-partners__item">
-                        <img src="'. $partner_item_logo_url .'" alt="partner logo">
-                    </div>';
+                foreach ($files as $item) {
+                    if (!empty($item["link"])) {
+                        $output .= '
+                        <div class="pwe-header-partners__item">
+                            <a href="'. $item["link"] .'" target"_blank"><img src="'. $item["url"] .'" alt="partner logo"></a>
+                        </div>';
+                    } else {
+                        $output .= '
+                        <div class="pwe-header-partners__item">
+                            <img src="'. $item["url"] .'" alt="partner logo">
+                        </div>';
+                    }
+                    
                 }
             $output .= '
-            </div>
+            </div> 
         </div>';
 
+        // Check if $pwe_header_other_partners contains valid JSON
         $other_partners_logotypes = self::json_decode($pwe_header_other_partners);
         if (is_array($other_partners_logotypes)) {
             foreach ($other_partners_logotypes as $item) {
+                $unique_id = PWECommonFunctions::id_rnd();
+
                 $pwe_header_other_partner_title = $item["pwe_header_partners_other_title"];
                 $pwe_header_other_partner_logo_ids = $item["pwe_header_partners_other_logotypes"];
+                $pwe_header_partners_other_logotypes_catalog = $item["pwe_header_partners_other_logotypes_catalog"];
+
+                // Ensure that the logotypes are properly exploded into an array
                 $pwe_header_other_partner_logo_ids = explode(',', $pwe_header_other_partner_logo_ids);
-        
-                if (count($pwe_header_other_partner_logo_ids) > 2) {
+
+                // Reinitialize $files for other partner logos
+                $other_files = [];
+
+                foreach ($pwe_header_other_partner_logo_ids as $logo_id) {
+                    $pwe_header_other_partner_logo = wp_get_attachment_url($logo_id);
+                    if ($pwe_header_other_partner_logo) {
+                        $other_files[] = $pwe_header_other_partner_logo;
+                    }
+                }
+
+                if (count($other_files) > 0) {
+                    foreach ($other_files as &$file) {
+                        $file = [
+                            'url' => $file,
+                            'desc_pl' => basename(dirname($file)),
+                            'desc_en' => '',
+                            'link' => ''
+                        ];
+                    }
+                    unset($file);
+                }
+
+                // Ensure that $pwe_header_partners_other_logotypes_catalog is a valid, non-empty value
+                if (!empty($cap_logotypes_data) && !empty($pwe_header_partners_other_logotypes_catalog)) {
+                    $other_logotypes_catalogs_array = explode(',', $pwe_header_partners_other_logotypes_catalog);
+                    foreach ($cap_logotypes_data as $logo_data) {
+                        if (in_array($logo_data->logos_type, $other_logotypes_catalogs_array)) {
+                            $saving_paths($other_files, $logo_data);
+                        }
+                    }
+                }
+
+                if (count($other_files) > 1) {
                     $output .= '
                     <style>
-                        .pwe-header-partners-container {
+                        .pwe-header-partners-container.partners-'. $unique_id .' {
                             max-width: 280px;
                         }
-                        .pwe-header-partners__items {
+                        .partners-'. $unique_id .' .pwe-header-partners__items {
                             flex-wrap: wrap;
                             flex-direction: row;
                             gap: 8px;
                         }
-                        .pwe-header-partners__item {
+                        .partners-'. $unique_id .' .pwe-header-partners__item {
                             max-width: 120px;
                         }
                         @media(max-width: 1200px) {
-                            .pwe-header-partners-container {
+                            .pwe-header-partners-container.partners-'. $unique_id .' {
                                 max-width: 240px;
                             }
-                            .pwe-header-partners__items {
+                            .partners-'. $unique_id .' .pwe-header-partners__items {
                                 flex-wrap: wrap;
                                 flex-direction: row;
                                 gap: 8px;
                             }
-                            .pwe-header-partners__item {
+                            .partners-'. $unique_id .' .pwe-header-partners__item {
                                 max-width: 100px;
                             }
                         }
                         @media(max-width: 1100px) {
-                            .pwe-header-partners-container {
+                            .pwe-header-partners-container.partners-'. $unique_id .' {
                                 max-width: 280px;
                             }
-                            .pwe-header-partners__item {
+                            .partners-'. $unique_id .' .pwe-header-partners__item {
                                 max-width: 120px;
                             }
                         }
@@ -178,18 +276,24 @@ $output .= '
                 }
 
                 $output .= '
-                <div class="pwe-header-partners-container">
+                <div class="pwe-header-partners-container partners-'. $unique_id .'">
                     <div class="pwe-header-partners__title">
-                        <h3>'. $pwe_header_other_partner_title .'</h3>
+                        <h3>' . $pwe_header_other_partner_title . '</h3>
                     </div>
                     <div class="pwe-header-partners__items">';
-                        foreach ($pwe_header_other_partner_logo_ids as $logo_id) {
-                            $pwe_header_other_partner_logo = wp_get_attachment_url($logo_id);
-                            $output .= '
-                            <div class="pwe-header-partners__item">
-                                <img src="'. $pwe_header_other_partner_logo .'" alt="partner logo">
-                            </div>';
-                        } 
+                        foreach ($other_files as $item) {
+                            if (!empty($item["link"])) {
+                                $output .= '
+                                <div class="pwe-header-partners__item">
+                                    <a href="'. $item["link"] .'" target"_blank"><img src="'. $item["url"] .'" alt="partner logo"></a>
+                                </div>';
+                            } else {
+                                $output .= '
+                                <div class="pwe-header-partners__item">
+                                    <img src="'. $item["url"] .'" alt="partner logo">
+                                </div>';
+                            }
+                        }
                     $output .= '
                     </div>
                 </div>';  
